a7ec1b108ac4f314c2461a01944cad9b15a5e40b
Arathi <arathipk98@gmail.com>
2026-05-18 18:05:21 +0530

    feat: add Cash Balance to DCR report with cash/non-cash breakdown

--- hatco_ext/hatco_ext/report/dcr_report/dcr_report.js
+++ hatco_ext/hatco_ext/report/dcr_report/dcr_report.js
@@ -1,3 +1,4 @@
+
 // Copyright (c) 2026, Aravind R and contributors
 // For license information, please see license.txt
 
@@ -9,37 +10,56 @@ frappe.query_reports["DCR Report"] = {
             "label": "Date",
             "fieldtype": "Date",
             "default": frappe.datetime.get_today(),
-            "reqd": 0 
+            "reqd": 0
         },
         {
             "fieldname": "type",
             "label": "Type",
             "fieldtype": "Select",
-            "options": "\nCash Sales\nCard Sales\nCredit Sales\nCash Purchases\nCard Purchases\nCredit Purchases\nSales Return\nPurchase Return\nCustomer Receipts\nSupplier Payments\nBank Receipts\nBank Payments\nCash Receipts\nCash Payments\nJournal Entry",
-            "reqd": 0 
+            "options": "\nCash Sales\nCard Sales\nCredit Sales\nCash Purchases\nCard Purchases\nCredit Purchases\nSales Return\nPurchase Return\nCustomer Receipts (Cash)\nCustomer Receipts\nSupplier Payments (Cash)\nSupplier Payments\nBank Receipts\nBank Payments\nCash Receipts\nCash Payments\nJournal Entry\nInternal Transfer\nCash Balance",
+            "reqd": 0
+        },
+        {
+            "fieldname": "company",
+            "label": "Company",
+            "fieldtype": "Link",
+            "options": "Company",
+            "reqd": 0
         },
         {
             "fieldname": "cost_center",
             "label": "Cost Center",
             "fieldtype": "Link",
-            "options": "Cost Center"
-        }
+            "options": "Cost Center",
+            "get_query": function() {
+             var company = frappe.query_report.get_filter_value("company");
+             if (company) {
+                 return {
+                     "filters": {
+                         "company": company
+                     }
+                 };
+             } else {
+                 return {};
+             }
+    }
+           }
     ],
-    
+
     // Types into a clickable link
 
     "formatter": function(value, row, column, data, default_formatter) {
+
         value = default_formatter(value, row, column, data);
-        
+
         if (column.fieldname === "type" && data && data.voucher_type && data.voucher_no) {
 
-            const doctype = data.voucher_type;
-            const docname = data.voucher_no;
-            
-            const regex = new RegExp(`(${docname})`, 'g');
-            value = value.replace(regex, `<a href="/app/${doctype.toLowerCase().replace(/\s+/g, '-')}/${docname}" style="color: #000000; text-decoration: underline;">${docname}</a>`);
+            return `<a href="/app/${frappe.router.slug(data.voucher_type)}/${data.voucher_no}"
+                    style="color:#000000; text-decoration:underline;">
+                        ${value}
+                    </a>`;
         }
-        
+
         return value;
     }
 };
